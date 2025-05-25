436b128f147364c69d5522fb622bf6b5e90c9c33
risocode <risoca.code@gmail.com>
2025-05-25 15:56:03 +0800

    Insertion of Ads

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1482729173853463" crossOrigin="anonymous"></script>
+      </head>
       <body className={inter.className}>
         <ThemeProvider>
           {children}
